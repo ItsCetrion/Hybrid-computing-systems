3c154074e4b3a36ff80b4f83188b5b98ef9c7529
ItsCetrion <goodplay2.0@mail.ru>
2025-10-07 12:51:36 +0700

    added test for Matrix class

--- work2/Tests/srs/matrix_multiply_test.cu
+++ work2/Tests/srs/matrix_multiply_test.cu
@@ -7,28 +7,28 @@
 #define EIGEN_NO_CUDA
 
 class MatrixMulTest : public ::testing::TestWithParam<std::tuple<std::size_t, std::size_t, std::size_t, float>> {
- protected:
-  bool matmul_test_impl(std::size_t rows_a, std::size_t cols_a, std::size_t cols_b, float tol) {
-    Eigen::MatrixXf a_target = Eigen::MatrixXf::Random(rows_a, cols_a);
-    Eigen::MatrixXf b_target = Eigen::MatrixXf::Random(cols_a, cols_b);
-    
-    Eigen::MatrixXf c_target = a_target * b_target;
-    
-    Matrix<float> a(rows_a, cols_a);
-    a.getDeviceMemoryBlock().copyFromHost(a_target.data());
-    
-    Matrix<float> b(cols_a, cols_b);
-    b.getDeviceMemoryBlock().copyFromHost(b_target.data());
-    
-    Matrix<float> c = a * b;
-    
-    if (c.rows() != rows_a || c.cols() != cols_b) return false;
-    
-    Eigen::MatrixXf c_from_device = Eigen::MatrixXf::Zero(rows_a, cols_b);
-    c.getDeviceMemoryBlock().copyToHost(c_from_device.data());
-    
-    return c_target.isApprox(c_from_device, tol);
-  }
+    protected:
+    bool matmul_test_impl(std::size_t rows_a, std::size_t cols_a, std::size_t cols_b, float tol) {
+        Eigen::MatrixXf a_target = Eigen::MatrixXf::Random(rows_a, cols_a);
+        Eigen::MatrixXf b_target = Eigen::MatrixXf::Random(cols_a, cols_b);
+        
+        Eigen::MatrixXf c_target = a_target * b_target;
+        
+        Matrix<float> a(rows_a, cols_a);
+        a.getDeviceMemoryBlock().copyFromHost(a_target.data());
+        
+        Matrix<float> b(cols_a, cols_b);
+        b.getDeviceMemoryBlock().copyFromHost(b_target.data());
+        
+        Matrix<float> c = a * b;
+        
+        if (c.rows() != rows_a || c.cols() != cols_b) return false;
+        
+        Eigen::MatrixXf c_from_device = Eigen::MatrixXf::Zero(rows_a, cols_b);
+        c.getDeviceMemoryBlock().copyToHost(c_from_device.data());
+        
+        return c_target.isApprox(c_from_device, tol);
+    }
 };
 
 TEST_P(MatrixMulTest, matmul_test) {
